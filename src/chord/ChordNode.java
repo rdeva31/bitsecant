1f33c52bfcc90a354c8bf30dc24c8b939b8a015d
mlentz743 <mlentz743@cbe1a73c-c8c9-869f-b83c-6b0043bc9354>
2010-05-05 02:42:58 +0000

    Fixed some major bugs, and implemented RUDP sockets into the chord shit.
    Needs more testing around fixFingers for correct hash calculation (Figure out byte order relative to what SHA 1 returns)

--- src/chord/ChordNode.java
+++ src/chord/ChordNode.java
@@ -11,10 +11,14 @@ public class ChordNode
 {
 	private byte[] hash;
 	private RUDPSocket sock;
+	private InetAddress IPAddr;
+	private short port;
 
 	public ChordNode(InetAddress IPAddr, short port) throws Exception
 	{
-		sock = new RUDPSocket(IPAddr, port);
+		sock = null;
+		this.IPAddr = IPAddr;
+		this.port = port;
 		
 		byte[] identifier = Arrays.copyOf(IPAddr.getAddress(), 6);
 		identifier[4] = (byte)((port >> 8) & 0xFF);
@@ -25,10 +29,12 @@ public class ChordNode
 	public ChordNode(RUDPSocket sock) throws Exception
 	{
 		this.sock = sock;
+		this.IPAddr = sock.getSockAddr().getAddress();
+		this.port = (short)sock.getSockAddr().getPort();
 		
-		byte[] identifier = Arrays.copyOf(sockAddr.getAddress().getAddress(), 6);
-		identifier[4] = (byte)((sockAddr.getPort() >> 8) & 0xFF);
-		identifier[5] = (byte)(sockAddr.getPort() & 0xFF);
+		byte[] identifier = Arrays.copyOf(sock.getSockAddr().getAddress().getAddress(), 6);
+		identifier[4] = (byte)((sock.getSockAddr().getPort() >> 8) & 0xFF);
+		identifier[5] = (byte)(sock.getSockAddr().getPort() & 0xFF);
 		hash = MessageDigest.getInstance("SHA").digest(identifier);
 	}
 	
@@ -37,84 +43,88 @@ public class ChordNode
 		return sock;
 	}
 	
+	public InetAddress getIPAddress()
+	{
+		return IPAddr;
+	}
+	
+	public short getPort()
+	{
+		return port;
+	}
+	
 	public byte[] getHash()
 	{
 		return hash;
 	}
 	
-	public InetAddress getIPAddress()
+	public void connect() throws Exception
 	{
-		return sockAddr.getAddress();
-	}
-	
-	public short getPort()
-	{
-		return (short)sockAddr.getPort();
+		if(sock == null)
+		{
+			sock = new RUDPSocket(IPAddr, port);
+		}
 	}
 	
 	public void close() throws Exception
 	{
-		sock.close();
-		sock = null;
+		if(sock != null)
+		{
+			sock.close();
+			sock = null;
+		}
 	}
 	
 	public void sendMessage(MessageType type, ByteBuffer payload) throws Exception
 	{
-		if(sock == null)
-		{
-			connect();
-		}
-
 		byte[] message;
 
 		if(payload == null)
 		{
-			message = new byte[5];
-
-			message[0] = 0;
-			message[1] = 0;
-			message[2] = 0;
-			message[3] = 1;
-
-			message[4] = (byte)type.valueOf();
+			message = new byte[1];
+			message[0] = (byte)type.valueOf();
 		}
 		else
 		{
 			payload.flip();
 
 			int messageLength = 1 + payload.remaining();
-			message = new byte[4 + messageLength];
-
-			message[0] = (byte)(messageLength >> 24);
-			message[1] = (byte)(messageLength >> 16);
-			message[2] = (byte)(messageLength >> 8);
-			message[3] = (byte)(messageLength);
-
-			message[4] = (byte)type.valueOf();
-
-			payload.get(message, 5, messageLength - 1);
+			
+			message = new byte[messageLength];
+			message[0] = (byte)type.valueOf();
+			payload.get(message, 1, messageLength - 1);
 		}
 		
-		sockOut.write(message, 0, message.length);
+		sock.write(message);
 	}
 	
 	public ByteBuffer getResponse() throws Exception
 	{
-		byte[] header = new byte[4];
-		sockIn.read(header, 0, 4);
-		
-		int messageLength = (((int)header[0] & 0xFF) << 24) + (((int)header[1] & 0xFF) << 16) + 
-							(((int)header[2] & 0xFF) << 8) + ((int)header[0] & 0xFF);
-							
-		byte[] message = new byte[messageLength];
-		sockIn.read(message, 0, messageLength);
-		
-		return ByteBuffer.wrap(message);
+		return sock.read();
+	}
+	
+	@Override
+	public boolean equals(Object obj)
+	{
+		if (obj == null)
+		{
+			return false;
+		}
+		if (getClass() != obj.getClass())
+		{
+			return false;
+		}
+		final ChordNode other = (ChordNode) obj;
+		if (!Arrays.equals(other.getHash(), hash))
+		{
+			return false;
+		}
+		return true;
 	}
 	
 	private static int compare(byte[] hash1, byte[] hash2)
 	{
-		for (int c = hash1.length-1; c >= 0; --c)
+		for (int c = 0; c < hash1.length; ++c)
 		{
 			if (hash1[c] > hash2[c])
 				return 1;
